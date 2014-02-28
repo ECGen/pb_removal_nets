5c587cc1194c3117a53492b011fcd1053e6a1ea4
mkl <mkl48@nau.edu>
2014-02-28 11:29:59 -0500

    prn nestedness scripts

--- src/nest09.R
+++ src/nest09.R
@@ -2,17 +2,21 @@ library(bipartite)
 source('../src/pbr_load_data_hoth.R')
 prn.09 <- list(c=pbr.09$c,x=pbr.09$x,cnpb=pbr.09$c[,colnames(pbr.09$c)!='pb'],xnpb=pbr.09$x[,colnames(pbr.09$x)!='pb'])
                                         #run nestedness
-nest09c <- oecosimu(prn.09$c,nestfun=nestedness,method='r1')
-nest09x <- oecosimu(prn.09$x,nestfun=nestedness,method='r1')
-nest09cnpb <- oecosimu(prn.09$cnpb,nestfun=nestedness,method='r1')
-nest09xnpb <- oecosimu(prn.09$xnpb,nestfun=nestedness,method='r1')
                                         #write results
 print('2009 Control')
+nest09c <- oecosimu(prn.09$c,nestfun=nestedness,method='r1')
 dput(nest09c,'../results/nest09c.results')
 print('2009 Exclusion')
+nest09x <- oecosimu(prn.09$x,nestfun=nestedness,method='r1')
 dput(nest09x,'../results/nest09x.results')
 print('2009 Control no pb')
+nest09cnpb <- oecosimu(prn.09$cnpb,nestfun=nestedness,method='r1')
 dput(nest09cnpb,'../results/nest09cnpb.results')
 print('2009 Exclusion no pb')
+nest09xnpb <- oecosimu(prn.09$xnpb,nestfun=nestedness,method='r1')
 dput(nest09xnpb,'../results/nest09xnpb.results')
+
+
+
+
 
