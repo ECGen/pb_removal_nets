2dcfcb5ebaaac850328bad412bf6c37fd015eb94
mkl <mkl48@nau.edu>
2014-02-28 11:23:11 -0500

    prn nestedness scripts

--- src/nest09.R
+++ src/nest09.R
@@ -7,8 +7,12 @@ nest09x <- oecosimu(prn.09$x,nestfun=nestedness,method='r1')
 nest09cnpb <- oecosimu(prn.09$cnpb,nestfun=nestedness,method='r1')
 nest09xnpb <- oecosimu(prn.09$xnpb,nestfun=nestedness,method='r1')
                                         #write results
+print('2009 Control')
 dput(nest09c,'../results/nest09c.results')
+print('2009 Exclusion')
 dput(nest09x,'../results/nest09x.results')
+print('2009 Control no pb')
 dput(nest09cnpb,'../results/nest09cnpb.results')
+print('2009 Exclusion no pb')
 dput(nest09xnpb,'../results/nest09xnpb.results')
 
