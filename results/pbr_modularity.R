f97505fbda06e313bfd52c3a12069f763c53fdea
mkl <mkl48@nau.edu>
2014-03-11 15:42:07 -0400

    removiing old results files

--- results/pbr_modularity.R
+++
@@ -1,3 +0,0 @@
-structure(list(null = c(0.218669, 0.226569), results = structure(c(0.209428, 
--2.36137862041311, 0, 1), .Names = c("obs", "z", "lower.p", "upper.p"
-))), .Names = c("null", "results"))
