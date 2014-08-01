d820865bcf554535f362b46f8b67c96d090fdb27
mkl <mkl48@nau.edu>
2014-08-01 12:49:27 -0400

    hoth modularity script prep

--- src/pbr_patmods/get_mod.R
+++ src/pbr_patmods/get_mod.R
@@ -4,6 +4,6 @@ library(bipartite)
 library(methods)
 args <- commandArgs()
 x <- read.csv(args[length(args)-1])
-y <- computeModules(x,steps=10)
+y <- computeModules(x,steps=100000)
 cat(paste('',slot(y,name='likelihood')),file=args[length(args)],append=TRUE)
 
