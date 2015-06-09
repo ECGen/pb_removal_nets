095e8b2026901e8d16a61fa9bdbebf9dc05b8ef5
MKLau <matthewklau@fas.harvard.edu>
2015-06-09 15:16:52 -0400

    update

--- src/global.R
+++ src/global.R
@@ -1,5 +1,36 @@
 library(bipartite)
 
+meanMat <- function(x,y){
+    (equalMat(x,y)[[1]] + equalMat(x,y)[[2]]) / 2
+}
+
+equalMat <- function(x,y){
+    x <- list(x,x)
+    y <- list(y,y)
+    x[[2]] <- matrix(0,nrow=nrow(x[[1]]),
+                           ncol=length(
+                               colnames(y[[1]])[colnames(y[[1]]) 
+                                                %in% 
+                                                colnames(x[[1]]) == FALSE])
+                     )
+    colnames(x[[2]]) <- colnames(y[[1]])[colnames(y[[1]]) 
+                                         %in% 
+                                         colnames(x[[1]]) == FALSE]
+    x[[2]] <- cbind(x[[1]],x[[2]])
+
+    y[[2]] <- matrix(0,nrow=nrow(y[[1]]),
+                           ncol=length(
+                               colnames(x[[1]])[colnames(x[[1]]) 
+                                                %in% 
+                                                colnames(y[[1]]) == FALSE])
+                     )
+    colnames(y[[2]]) <- colnames(x[[1]])[colnames(x[[1]]) 
+                                         %in% 
+                                         colnames(y[[1]]) == FALSE]
+    y[[2]] <- cbind(y[[1]],y[[2]])
+    return(list(x=x[[2]],y=y[[2]]))
+}
+
 mkNull <- function(x,n,null.dir){
     for (i in 1:n){
         write.table(nullmodel(x,method='r2d',N=1),
--- src/pbrNetAnalyses.R
+++ src/pbrNetAnalyses.R
@@ -2,35 +2,44 @@
 ### PB network modularity
 ##18Apr2015
 
+n.null <- 100
+
 library(bipartite)
 library(methods)
 source('global.R')
 source('pbrDataLoader.R')
 
-
-### compute modules without 
-
 ##1. Compute observed modularity
 ##2. Generate null set
 ##3. Calculate modularity for null set
 ##4. Calculate contribution to modularity for each tree
-##5. Cacluulate contribution to modularity for each tree
+##5. Caclulate contribution to modularity for each tree
 ##6. Repeat for each year
 ##7. Repeat using the average across years
 
-###Remove PB
-
+### Setup data
 pbr <- list(c08=pbr.08$c,x08=pbr.08$x,c09=pbr.09$c,x09=pbr.09$x)
-pbr <- lapply(pbr,function(x) x[,colnames(x)!='pb'])
+pbr.avg <- list(c.avg=meanMat(pbr$c08,pbr$c09),x.avg=meanMat(pbr$x08,pbr$x09))
+pbr.avg <- lapply(pbr.avg,function(x) apply(x,2,floor))
+pbr <- c(pbr,pbr.avg)
+pbr. <- lapply(pbr,function(x) x[,colnames(x)!='pb'])
+names(pbr.) <- paste(names(pbr.),'.npb',sep='')
+pbr <- c(pbr,pbr.)
+
+### Null communities
+null.dir <- paste("../results/npb",names(pbr),sep="/")
+
+for (i in 1:length(pbr)){
+    mkNull(pbr[[i]],n.null,null.dir[i])
+}
 
 ### Observed Modularity
 obs <- lapply(pbr,computeModules)
-
-### Null modules
-null.dir <- paste("npb",names(pbr),sep="/")
-for (i in 1:length(pbr)){
-    mkNull(pbr[[i]],5000,null.dir[i])
+for (i in 1:obs){
+    obs <- computeModules(pbr[[i]])
+    dput(obs,file=paste(null.dir,'/',names(obs)[[i]],'.rdata',sep=''))
 }
+
 ### Contribution to modularity
 
 ### Stats
